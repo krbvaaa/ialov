93c0561c618cd5cbb5368af7dfeb9aee159710bb
inkara <kairbaevainkara2020@gmail.com>
2024-12-01 14:04:37 +0500

    first commit

---
+++ new.py
@@ -0,0 +1,13 @@
+import colorama
+
+class Bmw:
+    pass
+
+def privet_function():
+    pass
+
+data = "string"
+print(hasattr( data, "reverse"))
+print(hasattr(data, "index"))
+print(callable(data))
+print(callable(privet_function))
